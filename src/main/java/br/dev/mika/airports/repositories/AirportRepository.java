76b2ea6284dd6b0ad9610db670da09e9c9dbe46c
Mika <mika@gmail.com>
2025-05-21 15:52:25 -0300

    Término da Fase 2 –Implementado endpoint Airport -Add interface AirportRepository -Add classe AirportService -Add classe AirportController

---
+++ src/main/java/br/dev/mika/airports/repositories/AirportRepository.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package br.dev.mika.airports.repositories;
+
+/**
+ *
+ * @author ppjatb
+ */
+public interface AirportRepository {
+    
+}
